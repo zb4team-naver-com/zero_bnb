0bf2e69127c9b5bfd25caa35fe835dad0db2cbaf
haze <skdml20022@gmail.com>
2022-09-16 18:18:17 +0900

    spring-react 연동 recommit

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Router from './router/Router'
+import axios from 'axios';
 
 function App() {
-  
+
+  useEffect(() => {
+    axios.get('/test')
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+  }, []);
+
   return <Router/>;
 }
 
-export default App
+export default App;
