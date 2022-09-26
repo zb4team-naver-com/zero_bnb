a6e5ddc912f0369ea8d6cd111213a23c262ea3ba
haze <skdml20022@gmail.com>
2022-09-26 15:48:50 +0900

    회원 정보 수정

--- src/main/java/com/service/zerobnb/web/guest/controller/GuestController.java
+++ src/main/java/com/service/zerobnb/web/guest/controller/GuestController.java
@@ -1,23 +1,28 @@
 package com.service.zerobnb.web.guest.controller;
 
 import com.service.zerobnb.web.guest.dto.GuestDto;
+import com.service.zerobnb.web.guest.model.GuestInput;
 import com.service.zerobnb.web.guest.service.GuestService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/guest")
 @RequiredArgsConstructor
-@Tag(name = "User", description = "모든 유저에 관련된 API 입니다.")
+@Tag(name = "User", description = "유저에 관련된 API 입니다.")
 public class GuestController {
 
     private final GuestService guestService;
@@ -26,11 +31,23 @@ public class GuestController {
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "유저 정보 데이터(JSON)")
     })
-    @PostMapping("/info")
-    public ResponseEntity<GuestDto> getUserInfo () {
+    @GetMapping("/{id}")
+    public ResponseEntity<GuestDto> getInfo(@PathVariable Long id) {
 
-        GuestDto guestDto = GuestDto.builder()
-            .build();
+        GuestDto guestDto = guestService.getGuestInfo(id);
+
+        return ResponseEntity.ok(guestDto);
+    }
+
+    @Operation(summary = "유저의 정보를 수정합니다.", description = "")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "유저의 수정된 정보 데이터(JSON)")
+    })
+    @PutMapping("/update/{id}")
+    public ResponseEntity<GuestDto> updateInfo(@RequestBody @Valid GuestInput request,
+                                                @PathVariable Long id) {
+
+        GuestDto guestDto = this.guestService.update(id, request);
 
         return ResponseEntity.ok(guestDto);
     }
--- src/main/java/com/service/zerobnb/web/guest/service/GuestService.java
+++ src/main/java/com/service/zerobnb/web/guest/service/GuestService.java
@@ -1,2 +1,54 @@
-package com.service.zerobnb.web.guest.service;public class GuestService {
+package com.service.zerobnb.web.guest.service;
+
+import static com.service.zerobnb.web.error.message.ExceptionMessage.*;
+
+import com.service.zerobnb.web.error.model.GuestException;
+import com.service.zerobnb.web.guest.domain.Guest;
+import com.service.zerobnb.web.guest.dto.GuestDto;
+import com.service.zerobnb.web.guest.model.GuestInput;
+import com.service.zerobnb.web.guest.repository.GuestRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class GuestService {
+
+    private final GuestRepository guestRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public GuestDto getGuestInfo(Long userId) {
+
+        Guest guest = guestRepository.findById(userId)
+            .orElseThrow(() -> new GuestException(NOT_EXIST_GUEST));
+
+        return GuestDto.builder()
+                    .email(guest.getEmail())
+                    .name(guest.getName())
+                    .phone(guest.getPhone())
+                    .birth(guest.getBirth())
+                    .profileImage(guest.getProfileImage())
+                    .isHost(guest.isHost())
+                    .point(guest.getPoint())
+                    .build();
+    }
+
+    @Transactional
+    public GuestDto update(Long id, GuestInput request) {
+        Guest guest = guestRepository.findById(id)
+                                    .orElseThrow(() -> new GuestException(NOT_EXIST_GUEST));
+
+        guestRepository.save(guest.from(request));
+
+        return GuestDto.builder()
+                    .email(request.getEmail())
+                    .password(request.getPassword())
+                    .name(request.getName())
+                    .birth(request.getBirth())
+                    .phone(request.getPhone())
+                    .profileImage(request.getProfileImage())
+                    .build();
+    }
 }
