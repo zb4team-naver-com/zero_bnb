096252fe7b6b05f5b619bdc0d42017772b469a20
MyungHwanKim <code.kmh9250@gmail.com>
2022-09-28 14:55:43 +0900

    메인페이지 숙소 정보 API url 주소 "/" -> "/main/"으로 변경

--- src/main/java/com/service/zerobnb/web/main/MainController.java
+++ src/main/java/com/service/zerobnb/web/main/MainController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ import java.util.List;
 @Tag(name = "메인", description = "메인페이지 숙소 정보 API")
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/main")
 @Slf4j
 public class MainController {
 
