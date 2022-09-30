2f1cf026453c53cc1eab5505ad1bcaace5c82256
MyungHwanKim <code.kmh9250@gmail.com>
2022-09-30 16:45:50 +0900

    test :  예약할 때 guest point 보다 더 큰 point 사용 시 exception test code 작성

--- src/test/java/com/service/zerobnb/web/reservation/service/ReservationServiceTest.java
+++ src/test/java/com/service/zerobnb/web/reservation/service/ReservationServiceTest.java
@@ -7,6 +7,7 @@ import com.service.zerobnb.web.error.model.ReservationException;
 import com.service.zerobnb.web.error.model.RoomException;
 import com.service.zerobnb.web.guest.domain.Guest;
 import com.service.zerobnb.web.guest.repository.GuestRepository;
+import com.service.zerobnb.web.payment.domain.Payment;
 import com.service.zerobnb.web.payment.model.PaymentForm;
 import com.service.zerobnb.web.reservation.domain.Reservation;
 import com.service.zerobnb.web.reservation.dto.ReservationDto;
@@ -58,6 +59,7 @@ class ReservationServiceTest {
                 .name("홍길동")
                 .phone("010-1111-2222")
                 .status(ACTIVE)
+                .point(500L)
                 .build();
         given(guestRepository.findByEmail(anyString()))
                 .willReturn(Optional.of(guest));
@@ -116,6 +118,7 @@ class ReservationServiceTest {
         assertEquals(3, reservation.getPeopleCount());
         assertEquals("홍길동", reservation.getBookerName());
         assertEquals("010-1111-2222", reservation.getBookerPhone());
+        assertEquals(0L, guest.getPoint());
         assertEquals(VEHICLE, reservation.getTransportationType());
         assertEquals(1000, reservation.getPaymentDto().getCouponCost());
         assertEquals(500, reservation.getPaymentDto().getPointCost());
@@ -256,6 +259,74 @@ class ReservationServiceTest {
     }
 
     @Test
+    @DisplayName("사용 포인트가 게스트 포인트보다 많은 경우 - 예약 실패")
+    void usePointGreaterThanGuestPointTest() {
+        // given
+        Guest guest = Guest.builder()
+                .id(1L)
+                .email("abc@naver.com")
+                .name("홍길동")
+                .phone("010-1111-2222")
+                .status(ACTIVE)
+                .point(0L)
+                .build();
+        given(guestRepository.findByEmail(anyString()))
+                .willReturn(Optional.of(guest));
+        Accommodation accommodation = Accommodation.builder()
+                .id(1L)
+                .name("경남 게스트 하우스")
+                .locationPosition(new LocationPosition(25.5, 30.0))
+                .address("경남 김해시")
+                .description("자연의 풍경을 즐겨주세요.")
+                .accommodationType(GUEST_HOUSE)
+                .build();
+        Room room = Room.builder()
+                .id(1L)
+                .standardPeople(2)
+                .maxPeople(3)
+                .roomCount(5)
+                .cost(100000)
+                .accommodation(accommodation)
+                .build();
+        given(roomRepository.findById(anyLong()))
+                .willReturn(Optional.of(room));
+        given(reservationRepository.save(any()))
+                .willReturn(Reservation.builder()
+                        .id(1L)
+                        .guest(guest)
+                        .room(room)
+                        .checkInTime(LocalDateTime.of(2022, 9, 20, 0, 0, 0))
+                        .checkOutTime(LocalDateTime.of(2022, 9, 25, 0, 0, 0))
+                        .days(5)
+                        .cost(100000)
+                        .peopleCount(3)
+                        .transportationType(VEHICLE)
+                        .bookerName(guest.getName())
+                        .bookerPhone(guest.getPhone())
+                        .build());
+
+        // when
+        PaymentForm paymentForm = PaymentForm.builder()
+                .couponCost(1000)
+                .pointCost(500)
+                .paymentMethod(CARD)
+                .build();
+        ReservationForm form = ReservationForm.builder()
+                .roomId(1L)
+                .checkInTime(LocalDateTime.of(2022, 9, 20, 0, 0, 0))
+                .checkOutTime(LocalDateTime.of(2022, 9, 25, 0, 0, 0))
+                .peopleCount(3)
+                .transportationType(VEHICLE)
+                .paymentForm(paymentForm)
+                .build();
+        GuestException exception = assertThrows(GuestException.class,
+                () -> reservationService.reservation("abc@naver.com", form));
+
+        // then
+        assertEquals(USE_POINT_GREATER_THAN_GUEST_POINT.message(), exception.getMessage());
+    }
+
+    @Test
     @DisplayName("예약을 취소할 경우 경우 - 예약 취소 성공")
     void cancelReservationTest() {
         // given
@@ -264,6 +335,7 @@ class ReservationServiceTest {
                 .email("abc@naver.com")
                 .name("홍길동")
                 .phone("010-1111-2222")
+                .point(500L)
                 .status(ACTIVE)
                 .build();
         given(guestRepository.findByEmail(anyString()))
@@ -283,6 +355,12 @@ class ReservationServiceTest {
                                 .id(1L)
                                 .guest(guest)
                                 .room(room)
+                                .payment(Payment.builder()
+                                        .id(1L)
+                                        .reservationCost(90000)
+                                        .pointCost(500L)
+                                        .couponCost(0L)
+                                        .build())
                                 .checkInTime(LocalDateTime.of(2222, 9, 26, 0, 0, 0))
                                 .checkOutTime(LocalDateTime.of(2222, 9, 30, 0, 0, 0))
                                 .days(5)
@@ -299,6 +377,7 @@ class ReservationServiceTest {
         reservationService.cancelReservation("abc@naver.com", 1L);
 
         // then
+        assertEquals(1000L, guest.getPoint());
     }
 
     @Test
